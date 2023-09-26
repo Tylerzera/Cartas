48d0e53c6802f1eae5c595b01f7653636e26e7f8
Tylerzeraa <nathankz2@hotmail.com>
2023-09-26 20:01:31 -0300

    Adicionar arquivos de projeto.

---
+++ Cartas/Controllers/CartasController .cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CartasController : ControllerBase
+{
+    private static List<CartaDoPapaiNoel> cartas = new List<CartaDoPapaiNoel>();
+
+    [HttpPost]
+    public IActionResult Post([FromBody] CartaDoPapaiNoel carta)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        if (carta.IdadeDaCrianca >= 15)
+        {
+            return BadRequest("Apenas crianças com menos de 15 anos podem enviar cartas.");
+        }
+
+        cartas.Add(carta);
+        carta.Id = cartas.Count; 
+
+        return CreatedAtAction(nameof(Get), new { id = carta.Id }, carta);
+    }
+
+    [HttpGet]
+    public IEnumerable<CartaDoPapaiNoel> Get()
+    {
+        return cartas;
+    }
+}
